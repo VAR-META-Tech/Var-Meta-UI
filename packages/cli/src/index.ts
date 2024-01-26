4dce521a6c833a58a4d14700c64b4d08b451b703
HoangPham99 <pqhoang.dev@gmail.com>
2024-01-26 15:16:48 +0700

    feat: init cli

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { Command, program } from 'commander';
+
+import { getPackageInfo } from './utils/get-package-info';
+import { add } from './commands/add';
+
+process.on('SIGINT', () => process.exit(0));
+process.on('SIGTERM', () => process.exit(0));
+
+async function main() {
+  const packageInfo = await getPackageInfo();
+
+  const program = new Command()
+    .name('var-meta-ui')
+    .description('add components and dependencies to your project')
+    .version(packageInfo.version || '1.0.0', '-v, --version', 'display the version number');
+
+  program.addCommand(add);
+  // program.addCommand(init).addCommand(add).addCommand(diff);
+
+  program.parse();
+}
+
+main();
